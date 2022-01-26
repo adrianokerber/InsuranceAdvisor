f2f067cfcad9c989dae6774fe943335f6262aec8
Adriano Kerber <kerberpro@gmail.com>
2022-01-26 15:55:15 -0300

    :sparkles: Add Domain project and base models

---
+++ src/InsuranceAdvisor.Domain/Models/ClientProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace InsuranceAdvisor.Domain.Models
+{
+    public class ClientProfile
+    {
+        public ClientProfile(int age, int dependents, HouseProfile house, int income, string martialStatus, List<int> riskQuestions, VehicleProfile vehicle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

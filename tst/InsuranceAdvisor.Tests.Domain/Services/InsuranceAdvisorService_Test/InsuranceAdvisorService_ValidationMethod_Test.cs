f0c466a4bbda644907415fd40644be02dde8efda
Adriano Kerber <kerberpro@gmail.com>
2022-01-28 22:38:13 -0300

    :white_check_mark: Add InsuranceAdvisorService success tests

--- tst/InsuranceAdvisor.Tests.Domain/Services/InsuranceAdvisorService_Test/InsuranceAdvisorService_ValidationMethod_Test.cs
+++ tst/InsuranceAdvisor.Tests.Domain/Services/InsuranceAdvisorService_Test/InsuranceAdvisorService_ValidationMethod_Test.cs
@@ -20,7 +20,120 @@ namespace InsuranceAdvisor.Tests.Domain.Services.InsuranceAdvisorService_Test
         }
 
         #region Success cases
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(19)]
+        [InlineData(29)]
+        [InlineData(208083)]
+        [Trait("Success", "")]
+        public void When_ClientHasValidAge_Expect_ValidResult(int givenAge)
+        {
+            // Arrange
+            var clientProfile = ClientProfileTestFactory.CreateValidClientProfileWithAge(givenAge);
 
+            // Act
+            var validationResult = _insuranceAdvisorService.ValidateClientProfile(clientProfile);
+
+            // Assert
+            validationResult.IsValid
+                .Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(1000)]
+        [Trait("Success", "")]
+        public void When_ClientHasValidDependents_Expect_ValidResult(int givenDependents)
+        {
+            // Arrange
+            var clientProfile = ClientProfileTestFactory.CreateValidClientProfileWithDependents(givenDependents);
+
+            // Act
+            var validationResult = _insuranceAdvisorService.ValidateClientProfile(clientProfile);
+
+            // Assert
+            validationResult.IsValid
+                .Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(100)]
+        [InlineData(899899)]
+        [InlineData(200_000)]
+        [Trait("Success", "")]
+        public void When_ClientHasValidIncome_Expect_ValidResult(int givenIncome)
+        {
+            // Arrange
+            var clientProfile = ClientProfileTestFactory.CreateValidClientProfileWithIncome(givenIncome);
+
+            // Act
+            var validationResult = _insuranceAdvisorService.ValidateClientProfile(clientProfile);
+
+            // Assert
+            validationResult.IsValid
+                .Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(MartialStatus.Single)]
+        [InlineData(MartialStatus.Married)]
+        [Trait("Success", "")]
+        public void When_ClientHasValidMartialStatus_Expect_ValidResult(MartialStatus givenMartialStatus)
+        {
+            // Arrange
+            var clientProfile = ClientProfileTestFactory.CreateValidClientProfileWithMartialStatus(givenMartialStatus);
+
+            // Act
+            var validationResult = _insuranceAdvisorService.ValidateClientProfile(clientProfile);
+
+            // Assert
+            validationResult.IsValid
+                .Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(1, 0, 0)]
+        [InlineData(0, 1, 0)]
+        [InlineData(0, 0, 1)]
+        [InlineData(1, 1, 1)]
+        [Trait("Success", "")]
+        public void When_ClientHasAllThreeRiskQuestionsAnsweredWithValuesFrom0To1_Expect_ValidResult(int givenRiskQuestion1, int givenRiskQuestion2, int givenRiskQuestion3)
+        {
+            // Arrange
+            var givenRiskQuestions = new List<int> { givenRiskQuestion1, givenRiskQuestion2, givenRiskQuestion3 };
+            var clientProfile = ClientProfileTestFactory.CreateValidClientProfileWithRiskQuestions(givenRiskQuestions);
+
+            // Act
+            var validationResult = _insuranceAdvisorService.ValidateClientProfile(clientProfile);
+
+            // Assert
+            validationResult.IsValid
+                .Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(OwnershipStatus.Owned)]
+        [InlineData(OwnershipStatus.Mortgaged)]
+        [Trait("Success", "")]
+        public void When_ClientHasHouseWithProperOwnershipStatus_Expect_ValidResult(OwnershipStatus givenOwnershipStatus)
+        {
+            // Arrange
+            var givenHouseProfile = new HouseProfile(givenOwnershipStatus);
+            var clientProfile = ClientProfileTestFactory.CreateValidClientProfileWithHouse(givenHouseProfile);
+
+            // Act
+            var validationResult = _insuranceAdvisorService.ValidateClientProfile(clientProfile);
+
+            // Assert
+            validationResult.IsValid
+                .Should().BeTrue();
+        }
         #endregion
 
         #region Error cases

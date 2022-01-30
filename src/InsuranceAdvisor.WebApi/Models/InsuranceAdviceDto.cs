ce2c2d992d97f8677a5c01e16066267028f47222
Adriano Kerber <kerberpro@gmail.com>
2022-01-30 16:29:18 -0300

    :truck: Rename files and folders failing on Docker

---
+++ src/InsuranceAdvisor.WebApi/Models/InsuranceAdviceDto.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InsuranceAdvisor.WebApi.Models
+{
+    public class InsuranceAdviceDto
+    {
+        [JsonPropertyName("auto")]
+        public string Auto { get; }
+        [JsonPropertyName("disability")]
+        public string Disability { get; }
+        [JsonPropertyName("home")]
+        public string Home { get; }
+        [JsonPropertyName("life")]
+        public string Life { get; }
+
+        public InsuranceAdviceDto(string auto,
+                                  string disability,
+                                  string home,
+                                  string life)
+        {
+            Auto = auto;
+            Disability = disability;
+            Home = home;
+            Life = life;
+        }
+    }
+}
